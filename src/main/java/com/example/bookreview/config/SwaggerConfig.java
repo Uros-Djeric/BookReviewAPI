9ed1b2f58a6f30d6ecafeb0fa56c478c701d27ec
Uros-Djeric <electronicmail.ds@gmail.com>
2025-10-12 23:52:30 +0200

    Added Swagger config file

---
+++ src/main/java/com/example/bookreview/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.example.bookreview.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Review API",
+                version = "1.0.0",
+                description = """
+                        REST API za upravljanje knjigama i recenzijama.
+                        
+                        Koristi JWT autentifikaciju, RequestSanitizer filter i centralizovano rukovanje greškama.
+                        """
+        ),
+        security = { @SecurityRequirement(name = "bearerAuth") }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Lokalno okruženje")
+                ))
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Book Review API")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+}
