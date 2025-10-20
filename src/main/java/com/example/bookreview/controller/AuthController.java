d39d17feeabef469e24728eac6ed7aa39b2357a5
Uros Djeric <udjeric@yettel.rs>
2025-10-20 13:56:53 +0200

    Updated swagger auth summary

--- src/main/java/com/example/bookreview/controller/AuthController.java
+++ src/main/java/com/example/bookreview/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.bookreview.controller;
 
 
 import com.example.bookreview.utils.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +21,10 @@ public class AuthController {
 
     private final JWTUtil jwtUtil;
 
+    @Operation(
+            summary = "First step! Generates a token",
+            description = "Insert any username and generate a token with which you can Authorize all method calls using the Authorize button!"
+    )
     @GetMapping
     public ResponseEntity<?> getJwtToken(@RequestParam String username) {
         String token = jwtUtil.generateToken(username);
